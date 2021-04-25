2f31e7e3c7386f490864fde83ce6387ceae476d5
Antti <antti@antti.codes>
2021-04-25 19:01:59 +0300

    fix: tabs

--- kossu/src/index.js
+++ kossu/src/index.js
@@ -4,22 +4,22 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// Called when activated
-	console.log('Testaustime activated!');
+    // Called when activated
+    console.log('Testaustime activated!');
 
-	// Must be defined in package.json
-	let disposable = vscode.commands.registerCommand('testaustime.test', () => {
-		vscode.window.showInformationMessage('Testaustime active!');
-	});
+    // Must be defined in package.json
+    let disposable = vscode.commands.registerCommand('testaustime.test', () => {
+        vscode.window.showInformationMessage('Testaustime active!');
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 function deactivate() {
-	// Called when deactivated
+    // Called when deactivated
 }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
