fede4befe7386f14f820cb58392b69a4ff445b80
ahnl <46541386+ahnl@users.noreply.github.com>
2021-04-26 15:10:09 +0300

    improvement(style): Change indent from 2 to 4

--- kossu/src/index.js
+++ kossu/src/index.js
@@ -5,63 +5,63 @@ const axios = require('axios');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const config = context.globalState;
-  let apikey = config.get('apikey'); // eslint-disable-line no-unused-vars
-  let endpoint = config.get('endpoint', 'https://time.testausserveri.fi/api/v1');
-  // Called when activated
-  console.log('Testaustime activated!');
+    const config = context.globalState;
+    let apikey = config.get('apikey'); // eslint-disable-line no-unused-vars
+    let endpoint = config.get('endpoint', 'https://time.testausserveri.fi/api/v1');
+    // Called when activated
+    console.log('Testaustime activated!');
 
-  // Must be defined in package.json
-  const test = vscode.commands.registerCommand('testaustime.test', () => {
-    vscode.window.showInformationMessage('Testaustime active!');
-  });
-  const setapikey = vscode.commands.registerCommand('testaustime.setapikey', async () => {
-    const result = await vscode.window.showInputBox({
-      placeHolder: 'Your API-key',
-      validateInput: (text) => (text.length !== 48 ? 'API keys are 24 bits!' : null),
+    // Must be defined in package.json
+    const test = vscode.commands.registerCommand('testaustime.test', () => {
+        vscode.window.showInformationMessage('Testaustime active!');
     });
-    if (!result || result.length !== 48) return;
-    vscode.window.showInformationMessage('Testing API-key...');
-    axios.post(`${endpoint}/user/validateAPIKey`, {
-      headers: {
-        Authorization: `Bearer ${result}`,
-      },
-    })
-      .then((res) => {
-        if (res.data.valid) {
-          config.update('apikey', result);
-          apikey = config.get('apikey');
-          vscode.window.showInformationMessage('API key set!');
-        } else {
-          vscode.window.showInformationMessage('API key invalid.');
-        }
-      })
-      .catch((e) => {
-        vscode.window.showInformationMessage(`Error checking the API key validity. ${e}`);
-      });
-  });
-
-  const setcustomapi = vscode.commands.registerCommand('testaustime.setcustomapi', async () => {
-    const result = await vscode.window.showInputBox({
-      placeHolder: 'https://time.testausserveri.fi/api/v1',
-      validateInput: (text) => (text.endsWith('/') ? 'Don\'t include the last slash' : null),
+    const setapikey = vscode.commands.registerCommand('testaustime.setapikey', async () => {
+        const result = await vscode.window.showInputBox({
+            placeHolder: 'Your API-key',
+            validateInput: (text) => (text.length !== 48 ? 'API keys are 24 bits!' : null),
+        });
+        if (!result || result.length !== 48) return;
+        vscode.window.showInformationMessage('Testing API-key...');
+        axios.post(`${endpoint}/user/validateAPIKey`, {
+            headers: {
+                Authorization: `Bearer ${result}`,
+            },
+        })
+            .then((res) => {
+                if (res.data.valid) {
+                    config.update('apikey', result);
+                    apikey = config.get('apikey');
+                    vscode.window.showInformationMessage('API key set!');
+                } else {
+                    vscode.window.showInformationMessage('API key invalid.');
+                }
+            })
+            .catch((e) => {
+                vscode.window.showInformationMessage(`Error checking the API key validity. ${e}`);
+            });
     });
-    if (!result || result.endsWith('/')) return;
-    config.update('endpoint', result);
-    endpoint = config.get('endpoint');
-    vscode.window.showInformationMessage('Endpoint key set!');
-  });
 
-  context.subscriptions.push(test);
-  context.subscriptions.push(setapikey);
-  context.subscriptions.push(setcustomapi);
+    const setcustomapi = vscode.commands.registerCommand('testaustime.setcustomapi', async () => {
+        const result = await vscode.window.showInputBox({
+            placeHolder: 'https://time.testausserveri.fi/api/v1',
+            validateInput: (text) => (text.endsWith('/') ? 'Don\'t include the last slash' : null),
+        });
+        if (!result || result.endsWith('/')) return;
+        config.update('endpoint', result);
+        endpoint = config.get('endpoint');
+        vscode.window.showInformationMessage('Endpoint key set!');
+    });
+
+    context.subscriptions.push(test);
+    context.subscriptions.push(setapikey);
+    context.subscriptions.push(setcustomapi);
 }
 
 function deactivate() {
-  // Called when deactivated
+    // Called when deactivated
 }
 
 module.exports = {
-  activate,
-  deactivate,
+    activate,
+    deactivate,
 };
--- makkara/src/index.js
+++ makkara/src/index.js
@@ -9,57 +9,57 @@ import config from '../config'; // eslint-disable-line import/no-unresolved,impo
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 const millisToMinutesAndSeconds = (millis) => {
-  const minutes = Math.floor(millis / 60000);
-  const seconds = ((millis % 60000) / 1000).toFixed(0);
-  return `${minutes}min ${(seconds < 10 ? '0' : '')}${seconds}s`;
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes}min ${(seconds < 10 ? '0' : '')}${seconds}s`;
 };
 
 const PREFIX = '.';
 client.on('message', async (message) => {
-  if (!message.content.startsWith(PREFIX) || message.content.length <= PREFIX.length) return;
-  const content = message.content.substring(PREFIX.length);
-  const args = content.split(' ');
-  const command = args.shift();
+    if (!message.content.startsWith(PREFIX) || message.content.length <= PREFIX.length) return;
+    const content = message.content.substring(PREFIX.length);
+    const args = content.split(' ');
+    const command = args.shift();
 
-  switch (command) {
+    switch (command) {
     case 'leaderboard': {
-      const msg = await message.reply('Haetaan listatietoja...');
-      axios.get(config.leaderboard_url)
-        .then((response) => {
-          let leaderboardString = '';
-          const { scoreboard } = response.data;
-          for (let i = 0; i < scoreboard.length; i += 1) {
-            leaderboardString += `${i + 1}. ${scoreboard[i].discordUser.tag} - ${millisToMinutesAndSeconds(scoreboard[i].totalTime)}\n`;
-          }
-          const embed = new Discord.MessageEmbed();
-          embed.setAuthor('time.testausserveri.fi');
-          embed.setDescription(leaderboardString);
-          embed.setTitle('Top 10');
-          msg.edit('', embed);
-        })
-        .catch((error) => { // eslint-disable-line no-unused-vars
-          message.reply('Virhe toplistaa haettaessa.');
-        });
-      break;
+        const msg = await message.reply('Haetaan listatietoja...');
+        axios.get(config.leaderboard_url)
+            .then((response) => {
+                let leaderboardString = '';
+                const { scoreboard } = response.data;
+                for (let i = 0; i < scoreboard.length; i += 1) {
+                    leaderboardString += `${i + 1}. ${scoreboard[i].discordUser.tag} - ${millisToMinutesAndSeconds(scoreboard[i].totalTime)}\n`;
+                }
+                const embed = new Discord.MessageEmbed();
+                embed.setAuthor('time.testausserveri.fi');
+                embed.setDescription(leaderboardString);
+                embed.setTitle('Top 10');
+                msg.edit('', embed);
+            })
+            .catch((error) => { // eslint-disable-line no-unused-vars
+                message.reply('Virhe toplistaa haettaessa.');
+            });
+        break;
     }
     case 'register': {
-      const embed = new Discord.MessageEmbed();
-      embed.setAuthor('time.testausserveri.fi');
-      embed.setDescription(`[Paina tästä](<${config.oauth_url}>) rekisteröityäksesi TestausTimeen.\nMuista kopioida API-key ja tallentaa se jonnekkin.`);
-      message.reply(embed);
-      break;
+        const embed = new Discord.MessageEmbed();
+        embed.setAuthor('time.testausserveri.fi');
+        embed.setDescription(`[Paina tästä](<${config.oauth_url}>) rekisteröityäksesi TestausTimeen.\nMuista kopioida API-key ja tallentaa se jonnekkin.`);
+        message.reply(embed);
+        break;
     }
     default: {
-      break;
+        break;
     }
-  }
+    }
 });
 
 client.login(config.discord_token).catch((e) => { // eslint-disable-line no-unused-vars
-  console.log('Login failed.');
-  process.exit();
+    console.log('Login failed.');
+    process.exit();
 });
