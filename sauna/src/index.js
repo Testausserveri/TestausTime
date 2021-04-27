d56dbcc457039ceb245206be54aaf05ce53a3098
awwry <79339968+awwry@users.noreply.github.com>
2021-04-27 16:20:47 +0300

    refactor: introduce dotenv and connectDatabase.js

--- sauna/src/index.js
+++ sauna/src/index.js
@@ -3,46 +3,29 @@
  * Track the time you spent coding, with a cool leaderboard.
  */
 import express from 'express';
-import mongoose from 'mongoose';
-import config from '../config.js';
-import bearer from 'express-bearer-token'; 
-import fs from 'fs';
+import bearer from 'express-bearer-token';
 
+import connectDatabase from './util/connectDatabase.js';
 import usersController from './controllers/users.js';
 import heartbeatController from './controllers/heartbeat.js';
 
-console.log('Connecting to the mongoDB');
-try {
-    await mongoose.connect(config.mongodb_connection_string, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    });
-}
-catch (error) {
-    console.error(error);
-    console.log('MongoDB connection failed');
-    process.exit();
-}
-console.log('Connected to the MongoDB. Starting web server');
+const port = 8080;
+
+console.log('Connecting to database.');
+await connectDatabase();
 
 const app = express();
 app.use(express.json());
 app.use(bearer());
 
-// Api endpoints
+// REST API endpoints
 app.use('/api/users', usersController);
 app.use('/api/heartbeat', heartbeatController);
 
 // React content
+app.use((_, res) => res.sendFile('index.html', {root: '../salmiakki/build/'}));
 app.use('/', express.static('../salmiakki/build/'));
-app.use((req, res) => {
-    res.sendFile('index.html', {root: '../salmiakki/build/'});
-});
 
-const PORT = 8080;
-
-app.listen(PORT, () => {
-    console.log(`Webserver up and running on port ${PORT}.`);
+app.listen(port, () => {
+    console.log(`Webserver up and running on port ${port}.`);
 });
