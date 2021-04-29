f954bfc879169881b36739d70fb0a93a626655ab
ahnl <46541386+ahnl@users.noreply.github.com>
2021-04-29 19:59:44 +0300

    fix(sauna): React serving and HTTP port

--- sauna/src/index.js
+++ sauna/src/index.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 import usersController from './controllers/users.js';
 import heartbeatController from './controllers/heartbeat.js';
 
-const port = 8080;
+const port = process.env.PORT || 80;
 const databaseUrl = process.env.MONGODB_URL;
 
 dotenv.config();
@@ -36,8 +36,8 @@ app.use('/api/users', usersController);
 app.use('/api/heartbeat', heartbeatController);
 
 // React content
-app.use((_, res) => res.sendFile('index.html', {root: '../salmiakki/build/'}));
 app.use('/', express.static('../salmiakki/build/'));
+app.use((_, res) => res.sendFile('index.html', {root: '../salmiakki/build/'}));
 
 app.listen(port, () => {
     console.log(`Webserver up and running on port ${port}.`);
