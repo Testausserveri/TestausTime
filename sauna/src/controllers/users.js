48a1b5dede8583d0f8f4375ce781e967c9efdf83
Esinko <34211401+Esinko@users.noreply.github.com>
2021-04-29 21:19:29 +0300

    fix: General fixes & Error handling for index.js
    - Some general fixes to style and variable names.
    - Imported mongoose.
    - Handled "reconnectFailed" event for mongoose.

--- sauna/src/controllers/users.js
+++ sauna/src/controllers/users.js
@@ -42,6 +42,7 @@ router.get('/register', async (req, res) => {
 
 /** Require given user to exist */
 router.use('/:id', async (req, res, next) => {
+    if (!req.params.id) return res.status(400).send('Expected id parameter as the user id.');
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
         return res.status(400).send('Invalid type object id.');
     }
--- sauna/src/index.js
+++ sauna/src/index.js
@@ -5,26 +5,36 @@
 import express from 'express';
 import bearer from 'express-bearer-token';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import usersController from './controllers/users.js';
 import heartbeatController from './controllers/heartbeat.js';
 
 const port = process.env.PORT || 80;
+const databaseUrl = process.env.MONGODB_URL;
 
 dotenv.config();
 
-console.log('Connecting to database.');
-await mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-})
-    .then(console.log('Connected to MongoDB database.'))
-    .catch((e) => {
-        console.log('Could not connect to MongoDB database: ', e);
+console.log('Connecting to database...');
+try {
+    if (!databaseUrl) throw new Error('No database url provided.');
+    const session = await mongoose.connect(databaseUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    });
+    console.log('Connected to database.');
+    session.connection.on('disconnected', () => {
+        console.warn('Disconnected from database. Reconnecting...');
+    });
+    session.connection.on('reconnectFailed', () => {
+        console.log('Disconnected and unable to reconnect to database.');
         process.exit(-1);
     });
+} catch (err) {
+    console.log(`Failed to connect to database\n${err}`);
+}
 
 const app = express();
 app.use(express.json());
@@ -36,7 +46,7 @@ app.use('/api/heartbeat', heartbeatController);
 
 // React content
 app.use('/', express.static('../salmiakki/build/'));
-app.use((_, res) => res.sendFile('index.html', {root: '../salmiakki/build/'}));
+app.use((_, res) => res.sendFile('index.html', { root: '../salmiakki/build/' }));
 
 app.listen(port, () => {
     console.log(`Webserver up and running on port ${port}.`);
